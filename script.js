70341c1dd990229299bf82b23ce4d8c2b958130e
time-sumeet <82651782+time-sumeet@users.noreply.github.com>
2022-05-09 22:22:50 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<script language="javascript">
+<!--hide
+var password;
+var pass1="12345";
+password=prompt('Enter Correct Password',' ');
+if (password==pass1)
+alert('Correct Password, Click OK to enter.');
+else
+{
+windows.location="http://www.google.com";
+}
+//-->
+</script>
